077c926e62c6d6ef965cbd1ad285d4efdfa9731f
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-10-17 17:27:10 +0200

    `Added` config file

---
+++ src/data/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    logo: 'https://i.imgur.com/5a1x3LP.png',
+    links: {
+        invite: `https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=8&scope=bot`,
+        support: 'https://go.fyxren.com/dc/mcdata'
+    },
+    ownerId: '462914535351779328'
+};
