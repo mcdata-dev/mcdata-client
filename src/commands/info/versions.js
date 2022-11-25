46573ea5431efb18614422094fd6de4238e519bb
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-25 20:39:51 +0100

    `Add - Cmd` versions

---
+++ src/commands/info/versions.js
@@ -0,0 +1,77 @@
+const { ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+const versions = require('../../data/mc/versions.json');
+
+module.exports = {
+    name: 'versions',
+    description: 'Get informations about version(s).',
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'version',
+            description: 'The Minecraft version.',
+            type: ApplicationCommandOptionType.String,
+            required: false
+        }
+    ],
+    run: async (client, interaction) => {
+
+        let query = interaction.options.get('version')?.value;
+
+        if (query) {
+            let v = versions.data.find(x => x.version === query);
+            if (!v) return interaction.reply(client.embeds.fail('This version does not exist.'));
+
+            let embed = new EmbedBuilder({
+                title: `Version | ${query}`,
+                color: client.c.main,
+                thumbnail: {
+                    url: client.config.logo
+                },
+                fields: [
+                    {
+                        name: 'Major',
+                        value: v.major,
+                        inline: true
+                    },
+                    {
+                        name: 'Major Name',
+                        value: v.majorName,
+                        inline: true
+                    },
+                    {
+                        name: 'Release',
+                        value: v.planned ? `Planned: ${v.planned}` : v.release,
+                        inline: true
+                    }
+                ]
+            });
+
+            return interaction.reply({ embeds: [embed] });
+
+        } else {
+
+            let arr = [];
+            await versions.data.forEach(val => {
+                arr.push(`\`${val.version}\``);
+            });
+
+            let embed = new EmbedBuilder({
+                title: `Versions`,
+                color: client.c.main,
+                thumbnail: {
+                    url: client.config.logo
+                },
+                description: '> _Excl. pre-releases etc._',
+                fields: [
+                    {
+                        name: 'List',
+                        value: arr.join(', ')
+                    }
+                ]
+            });
+
+            return interaction.reply({ embeds: [embed] });
+
+        }
+    }
+};
