aeb0cb906354ebbba1383afb6b74ed991e5beda7
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-27 21:04:09 +0100

    `Add` badges list

---
+++ src/data/config/badges.js
@@ -0,0 +1,55 @@
+module.exports = {
+    list: [
+        'dev',
+        'vip',
+        'mod',
+        'kofi',
+        'donator',
+        'translator',
+        'friend'
+    ],
+    data: [
+        {
+            'level': 1,
+            'id': 'dev',
+            'name': 'Developer',
+            'badge': '<:Ex_devBadge:817346861802192937>'
+        },
+        {
+            'level': 2,
+            'id': 'vip',
+            'name': 'VIP',
+            'badge': '<:Ex_vipBadge:817346862075478046>'
+        },
+        {
+            'level': 3,
+            'id': 'mod',
+            'name': 'Moderator',
+            'badge': '<:Ex_modBadge:817346862247706644>'
+        },
+        {
+            'level': 4,
+            'id': 'kofi',
+            'name': 'Ko-fi Donator',
+            'badge': '<:Ex_kofiBadge:818075915140988970>'
+        },
+        {
+            'level': 5,
+            'id': 'donator',
+            'name': 'Donator',
+            'badge': '<:Ex_donaterBadge:817346862146256916> '
+        },
+        {
+            'level': 6,
+            'id': 'translator',
+            'name': 'Translator',
+            'badge': '<:Ex_translatorBadge:818083420563308576>'
+        },
+        {
+            'level': 7,
+            'id': 'friend',
+            'name': 'Friend',
+            'badge': '<:Ex_friendBadge:817346862066434058>'
+        }
+    ]
+};
