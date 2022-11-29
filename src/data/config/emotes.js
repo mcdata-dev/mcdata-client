2f3f6cff2153d1fb2f5d46657061fd62e14e587b
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-29 22:13:41 +0100

    `Update` Finally put it the custom emojis

--- src/data/config/emotes.js
+++ src/data/config/emotes.js
@@ -1,6 +1,6 @@
 module.exports = {
-    done: '✅ | ',      // Success
-    fail: '❌ | ',      // User error, e.g missing argument
-    error: '⚠️ | ',     // Non-user error, e.g a bug or database issue
-    pending: '⏱️ | '    // If you have to wait for something (meant for embeds which will be editted)
-}
+    done: '<a:mc_done:1046521441870417981> | ',      // Success
+    fail: '<a:mc_fail:1046521445087461498> | ',      // User error, e.g missing argument
+    error: '<:mc_error:1046521443506204733> | ',     // Non-user error, e.g a bug or database issue
+    pending: '<a:mc_spinner:1046521366570078210> | '    // If you have to wait for something (meant for embeds which will be editted)
+};
