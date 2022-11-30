92c8fcbab0fd8655d49e4c8afeed8aa104dd1ec4
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-30 15:52:39 +0100

    `Update` config.links

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -2,7 +2,12 @@ module.exports = {
     logo: 'https://i.imgur.com/yiwM0p9.jpg',
     links: {
         invite: `https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=8&scope=bot`,
-        support: 'https://go.fyxren.com/dc/mcdata'
+        support: 'https://fyxren.com', //! TK: Update
+        kofi: 'https://ko-fi.com/Fyxren',
+        docs: 'https://mcdata.fyxren.com',
+        tos: 'https://mcdata.fyxren.com/miscellaneous/terms-of-service',
+        privacy: 'https://mcdata.fyxren.com/miscellaneous/privacy',
+        changelog: 'https://mcdata.fyxren.com/miscellaneous/changelog'
     },
     ownerId: '462914535351779328'
 };
