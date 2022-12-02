7c1d4318413f30e30a12978c4adb8116753abff2
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-02 21:08:58 +0100

    `Add - Cmd` uuid

---
+++ src/commands/profile/uuid.js
@@ -0,0 +1,53 @@
+const { ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+const PlayerManager = require('../../managers/PlayerManager');
+
+module.exports = {
+    name: 'uuid',
+    description: 'Get the UUID from a Minecraft user.',
+    category: 'profile',
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'username',
+            description: 'A minecraft username.',
+            type: ApplicationCommandOptionType.String,
+            required: false
+        }
+    ],
+    run: async (client, interaction) => {
+
+        let query = interaction.options.get('username')?.value;
+        if (!query) query = 'Fyxren';
+
+        if (query) {
+
+            let player = new PlayerManager(query);
+            player.usernameToUUID().then((data) => {
+                if (data.status !== 200) return interaction.reply(client.embeds.fail(`\`[${data.code}]\` ${data.msg}`));
+
+                let embed = new EmbedBuilder({
+                    title: `Username >> UUID`,
+                    color: client.c.main,
+                    thumbnail: {
+                        url: `https://crafatar.com/avatars/${data.id}`
+                    },
+                    fields: [
+                        {
+                            name: 'Username',
+                            value: data.name,
+                            inline: true
+                        },
+                        {
+                            name: 'UUID',
+                            value: data.id,
+                            inline: true
+                        }
+                    ]
+                });
+                return interaction.reply({ embeds: [embed] });
+            });
+
+        }
+
+    }
+};
