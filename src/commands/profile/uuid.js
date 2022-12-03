6732fbb03fd5461ecd4dd776ba4b69e197cbd190
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-03 21:49:03 +0100

    `Feature - Head` check on uuid

--- src/commands/profile/uuid.js
+++ src/commands/profile/uuid.js
@@ -17,38 +17,61 @@ module.exports = {
     run: async (client, interaction) => {
 
         let query = interaction.options.get('username')?.value;
-        if (!query) return interaction.reply(client.embeds.fail('You have to provide a username for now..'));
-
-        if (query) {
-
-            let player = new PlayerManager(query);
-            player.usernameToUUID().then((data) => {
-                if (data.status !== 200) return interaction.reply(client.embeds.fail(`\`[${data.satus}]\` ${data.msg}`));
-                let embed = new EmbedBuilder({
-                    title: `Username >> UUID | Fyxren`,
-                    color: client.c.main,
-                    thumbnail: {
-                        url: `https://crafatar.com/avatars/${data.id}`
-                    },
-                    footer: client.config.footer,
-                    timestamp: Date.now(),
-                    fields: [
-                        {
-                            name: 'UUID',
-                            value: data.id,
-                            inline: true
-                        },
-                        {
-                            name: 'Cached',
-                            value: `<t:${data.cachedAt}:R>`,
-                            inline: true
-                        }
-                    ]
-                });
-                return interaction.reply({ embeds: [embed] });
+        if (!query) {
+            let profile = await client.prisma.profile.findUnique({
+                where: { userId: interaction.user.id }
             });
 
+            if (!profile.uuid) return interaction.reply(client.embeds.fail('You don\'t have a linked account. Link your account or provide a username.'));
+            let embed = new EmbedBuilder({
+                title: `Username >> UUID | Fyxren`,
+                color: client.c.main,
+                thumbnail: {
+                    url: `https://crafatar.com/avatars/${profile.uuid}`
+                },
+                footer: client.config.footer,
+                timestamp: Date.now(),
+                fields: [
+                    {
+                        name: 'UUID',
+                        value: profile.uuid,
+                        inline: true
+                    },
+                    {
+                        name: 'Cached',
+                        value: '`-`',
+                        inline: true
+                    }
+                ]
+            });
+            return interaction.reply({ embeds: [embed] });
         }
 
+        let player = new PlayerManager(query);
+        player.usernameToUUID().then((data) => {
+            if (data.status !== 200) return interaction.reply(client.embeds.fail(`\`[${data.satus}]\` ${data.msg}`));
+            let embed = new EmbedBuilder({
+                title: `Username >> UUID | Fyxren`,
+                color: client.c.main,
+                thumbnail: {
+                    url: `https://crafatar.com/avatars/${data.id}`
+                },
+                footer: client.config.footer,
+                timestamp: Date.now(),
+                fields: [
+                    {
+                        name: 'UUID',
+                        value: data.id,
+                        inline: true
+                    },
+                    {
+                        name: 'Cached',
+                        value: `<t:${data.cachedAt}:R>`,
+                        inline: true
+                    }
+                ]
+            });
+            return interaction.reply({ embeds: [embed] });
+        });
     }
 };
