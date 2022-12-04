17daaef750e9781709522f32ed94b44892af68c4
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-04 21:04:11 +0100

    `Fix` UUID cmd showing "Fyxren" instead of dynamic name

--- src/commands/profile/uuid.js
+++ src/commands/profile/uuid.js
@@ -25,35 +25,39 @@ module.exports = {
             });
 
             if (!profile.uuid) return interaction.reply(client.embeds.fail('You don\'t have a linked account. Link your account or provide a username.'));
-            let embed = new EmbedBuilder({
-                title: `Username >> UUID | Fyxren`,
-                color: client.c.main,
-                thumbnail: {
-                    url: `https://crafatar.com/avatars/${profile.uuid}`
-                },
-                footer: client.config.footer,
-                timestamp: Date.now(),
-                fields: [
-                    {
-                        name: 'UUID',
-                        value: profile.uuid,
-                        inline: true
+
+            let player = new PlayerManager(profile.uuid);
+            player.UUIDToUsername().then((data) => {
+                let embed = new EmbedBuilder({
+                    title: `Username >> UUID | ${data.name}`,
+                    color: client.c.main,
+                    thumbnail: {
+                        url: `https://crafatar.com/avatars/${profile.uuid}`
                     },
-                    {
-                        name: 'Cached',
-                        value: '`-`',
-                        inline: true
-                    }
-                ]
+                    footer: client.config.footer,
+                    timestamp: Date.now(),
+                    fields: [
+                        {
+                            name: 'UUID',
+                            value: profile.uuid,
+                            inline: true
+                        },
+                        {
+                            name: 'Cached',
+                            value: '`-`',
+                            inline: true
+                        }
+                    ]
+                });
+                return interaction.reply({ embeds: [embed] });
             });
-            return interaction.reply({ embeds: [embed] });
         }
 
         let player = new PlayerManager(query);
         player.usernameToUUID().then((data) => {
             if (data.status !== 200) return interaction.reply(client.embeds.fail(`\`[${data.satus}]\` ${data.msg}`));
             let embed = new EmbedBuilder({
-                title: `Username >> UUID | Fyxren`,
+                title: `Username >> UUID | ${data.name}`,
                 color: client.c.main,
                 thumbnail: {
                     url: `https://crafatar.com/avatars/${data.id}`
