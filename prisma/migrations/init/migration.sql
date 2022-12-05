9a3746e3523a33bb09a1d955b3b5fcf1c7dd95d1
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-05 16:28:31 +0100

    `Update - Prisma` linkedSince Date >> Int

--- prisma/migrations/init/migration.sql
+++ prisma/migrations/init/migration.sql
@@ -12,10 +12,11 @@ CREATE TABLE `Profile` (
     `userId` VARCHAR(20) NOT NULL,
     `uuid` VARCHAR(100) NULL,
     `desc` VARCHAR(255) NULL,
-    `linkedSince` DATE NULL,
+    `linkedSince` INTEGER NULL,
 
     PRIMARY KEY (`userId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `Badge` ADD CONSTRAINT `Badge_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Profile`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
