3f2b81a979c88ea74bd8b46e6e9f2aa7d5fb3ac8
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-09 20:15:26 +0100

    `Update` config invite link

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
         text: 'McData'
     },
     links: {
-        invite: `https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=8&scope=bot`,
+        invite: `https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=274878203904&scope=bot%20applications.commands`,
         support: 'https://go.fyxren.com/support',
         kofi: 'https://ko-fi.com/Fyxren',
         docs: 'https://github.com/McData-Development/McData/wiki',
