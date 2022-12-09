21422730c90b0e007eb75a9283e65dbc0ffae7ae
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-09 18:52:24 +0100

    `Update` config links & `Remove` ownerId (bc .env)

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -6,12 +6,11 @@ module.exports = {
     },
     links: {
         invite: `https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=8&scope=bot`,
-        support: 'https://fyxren.com', //! TK: Update
+        support: 'https://go.fyxren.com/support',
         kofi: 'https://ko-fi.com/Fyxren',
-        docs: 'https://mcdata.fyxren.com',
-        tos: 'https://mcdata.fyxren.com/miscellaneous/terms-of-service',
-        privacy: 'https://mcdata.fyxren.com/miscellaneous/privacy',
-        changelog: 'https://mcdata.fyxren.com/miscellaneous/changelog'
-    },
-    ownerId: '462914535351779328'
+        docs: 'https://github.com/McData-Development/McData/wiki',
+        tos: 'https://github.com/McData-Development/McData/wiki/tos',
+        privacy: 'https://github.com/McData-Development/McData/wiki/privacy',
+        changelog: 'https://github.com/McData-Development/McData/wiki/changelog'
+    }
 };
