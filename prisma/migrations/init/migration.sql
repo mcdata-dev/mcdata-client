86bcd3c1319333bc03a694fc624bf18c0580cf99
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-12-12 16:25:54 +0100

    `Fix - MySQL` lowercase table names

--- prisma/migrations/init/migration.sql
+++ prisma/migrations/init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Badge` (
+CREATE TABLE `badge` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` VARCHAR(191) NOT NULL,
     `badge` VARCHAR(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE `Badge` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Profile` (
+CREATE TABLE `profile` (
     `userId` VARCHAR(20) NOT NULL,
     `uuid` VARCHAR(100) NULL,
     `desc` VARCHAR(255) NULL,
@@ -18,5 +18,5 @@ CREATE TABLE `Profile` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Badge` ADD CONSTRAINT `Badge_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Profile`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `badge` ADD CONSTRAINT `badge_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `profile`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
